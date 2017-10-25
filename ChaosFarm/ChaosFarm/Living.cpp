3ff1a41d2c76419dbd106537dad7c41c5b18c9a4
Yuka <Yuka>
2017-10-25 13:00:40 +0800

    Yuka, 2017-25-13:00, debug and add a new subclass FarmProduct.

--- ChaosFarm/ChaosFarm/Living.cpp
+++ ChaosFarm/ChaosFarm/Living.cpp
@@ -1,13 +1,14 @@
 #include "Living.h"
 
 
-Living::Living(vector<Abstract*>* abs_list, int size, int max_age, string* name, SEX sex) 
+Living::Living(vector<Abstract*>* abs_list, int size, int max_age, const char* name, SEX sex) 
 	:Entity(abs_list, size), age_(0), max_age_(max_age), grow_speed_(1.0), health_gradient_(0), health_(100), sex_(sex){
-	name_ = new string(name->c_str());
+	name_ = new string(name);
 
 }
 
 
-void Living::set_name(string* new_name){
-	*name_ = (*new_name);
+void Living::set_name(const char* new_name){
+	delete name_;
+	name_ = new string(new_name);
 }
--- ChaosFarm/ChaosFarm/main.cpp
+++ ChaosFarm/ChaosFarm/main.cpp
@@ -3,6 +3,8 @@
 
 int main(){
 	Time* time = Time::instance();
+	int test[] = { 2017, 15, 12, 15 };
+	time->set_time(test);
 	time->report();
 	system("pause");
 	return 0;
