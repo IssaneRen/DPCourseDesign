5b8f26dd37f611dac854dc66b10acc1cfb8106a3
Issane <renhongfeiasd@gmail.com>
2017-10-25 23:21:55 +0800

    delete all codes

--- ChaosFarm/ChaosFarm/Entity.cpp
+++
@@ -1,9 +0,0 @@
-#include "Entity.h"
-
-Entity::Entity(vector<Abstract*>* abs_list, int size):size_(size){
-	if (!abs_list->empty()){
-		for (vector<Abstract*>::iterator it = abs_list->begin(); it != abs_list->end(); it++){
-			(*it)->add_observer(this);
-		}
-	}
-}
