c8f794893ac2fe69a5e88c95c1b3d2b0a9ee046a
miao miao <634674250@qq.com>
2017-10-26 19:53:28 +0800

    添加crop 及plant类，ignore .opensdf

---
+++ ChaosFarm/ChaosFarm/Plant.cpp
@@ -0,0 +1,6 @@
+#include "Plant.h"
+
+Plant::Plant(vector<Abstract*>* abs_list, int size, int max_age, string* name, SEX sex)
+:Living(abs_list, size, max_age, name, sex), energy_(50), water_content_(50){
+
+}
