76173f6fe72e431a9aee17e9af76b431390ede3c
Issane <renhongfeiasd@gmail.com>
2017-10-27 19:22:48 +0800

    修改了main函数的测试方式，并且在其中用注释添加了以后测试的格式

--- ChaosFarm/ChaosFarm/main.cpp
+++ ChaosFarm/ChaosFarm/main.cpp
@@ -4,54 +4,37 @@
 #include "Insect.h"
 #include "Bee.h"
 
+void testTime();
+void testInsect();
+
+
 int main(){
+	//add Test Functions here!
+	//Do not delete test Functions
+	//instead, delete invoking them here in main() if you want to
+
+	testTime();
+	testInsect();
+
+	system("pause");
+	return 0;
+}
+
+void testTime() 
+{
 	Time* time = Time::instance();
 	int test[] = { 2017, 15, 12, 15 };
 	time->set_time(test);
 	time->report();
+}
 
+void testInsect() 
+{	
 	InsectGroup* beeGroup = new InsectGroup(new string("bee"));
 	beeGroup->hatch(100, NULL, 0, 0, NON);
 	cout << beeGroup->size() << endl;
 
 	InsectIterator* it = beeGroup->create_iterator();
-
-	//it = it->next();
-
 	Insect* temp = (Insect*)it->value()->get_value();
-
 	temp->cry();
-	/*
-	Farmland *f1 = new Redland();
-	f1 = new Kcl(f1);
-	f1 = new Kcl(f1);
-	f1 = new Nh4(f1);
-	cout << "description: " << f1->get_discription() << endl;;
-	cout << "size: " << f1->size() << endl;
-
-	cout << "-----------------------" << endl;
-
-	Farmland *f2 = new Blackland();
-	f2 = new Nh4(f2);
-	f2 = new Kcl(f2);
-	cout << "description: " << f2->get_discription() << endl;;
-	cout << "size: " << f2->size() << endl;
-
-	cout << "-----------------------" << endl;
-
-	Control *remote = new Control();
-	Farmland *f3 = new Farmland();
-	Plow *plow = new Plow(f3);
-	Smell *smell = new Smell(f3);
-	Ftime *time = new Ftime(f3);
-	remote->set_command(0, plow);
-	remote->set_command(1, smell);
-	remote->set_command(2, time);
-	remote->button_is_pressed(0);
-	remote->button_is_pressed(1);
-	remote->button_is_pressed(2);
-	remote->button_is_pressed(2);
-	*/
-	system("pause");
-	return 0;
 }
